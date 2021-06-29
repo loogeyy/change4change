4ce4fa87a2903d68cb21304f8210a2e3492cac62
Lucy Xu <lucythexu@gmail.com>
2021-06-29 17:35:08 -0400

    table updated

--- change4change/OrganizationListViewController.swift
+++ change4change/OrganizationListViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class OrganizationListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //REPLACE PLACEHOLDERS WITH ACTUAL CHARITY NAMES
+    let organizationNames = ["name1", "name2", "name3"]
+    
+    let organizationDescriptions = ["description1", "description2", "desription 3"]
+    
     override func viewDidLoad() {
             super.viewDidLoad()
 
@@ -21,14 +26,14 @@ class OrganizationListViewController: UIViewController, UITableViewDelegate, UIT
     
     //tells us how many rows are in the section
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 15 //puts in 15 rows for now
+            return organizationNames.count //puts in numbers of rows of organizations available
         }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
         
-        cell.mainLabel.text = "Org Name"
-        cell.descriptionLabel.text="Org Description"
+        cell.mainLabel.text = organizationNames[indexPath.row]
+        cell.descriptionLabel.text=organizationDescriptions[indexPath.row]
         cell.selectButton.tag=indexPath.row
         cell.selectButton.addTarget(self, action: #selector(rowButtonWasTapped(sender:)), for: .touchUpInside)
         //cell.OrgImageView.image=UIImage(named: "String")
