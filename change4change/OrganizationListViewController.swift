3a179638075207921c53dca6b6e44cfa78211806
Lucy Xu <lucythexu@gmail.com>
2021-06-29 17:04:20 -0400

    table placeholders done

--- change4change/OrganizationListViewController.swift
+++ change4change/OrganizationListViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class OrganizationListViewController: UIViewController {
-
+class OrganizationListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0 // must be float but returns cell height (each cell will be 100 px)
     }
+    
+    //tells us how many rows are in the section
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 15 //puts in 15 rows for now
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
+        
+        cell.mainLabel.text = "Org Name"
+        cell.descriptionLabel.text="Org Description"
+        cell.selectButton.tag=indexPath.row
+        cell.selectButton.addTarget(self, action: #selector(rowButtonWasTapped(sender:)), for: .touchUpInside)
+        //cell.OrgImageView.image=UIImage(named: "String")
+        
+        return cell
+    }
+    
+    @objc
+    func rowButtonWasTapped(sender:UIButton){
+        let rowIndex:Int=sender.tag
+        print(rowIndex)
+        
+    }
+    
+    
+    
+    
+
+    
     
 
     /*
